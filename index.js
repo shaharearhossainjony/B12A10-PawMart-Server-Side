52141b2f5358b271080f4743a45511b54e282435
shaharearhossainjony <shaharearhossainjony@gmail.com>
2025-12-07 20:53:28 +0600

    Database defined

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@b12a10-server-cluster.hig91a4.mongodb.net/?appName=B12A10-Server-Cluster`;
 
@@ -17,19 +16,25 @@ const client = new MongoClient(uri);
 async function run() {
   try {
     await client.connect();
+
+    const database = client.db("pawMart");
+    const petSuppliesCollection = database.collection("petSupplies");
+    const ordersCollection = database.collection("orders");
+
+
+
+    
+
     console.log("Connected to MongoDB successfully!");
   } finally {
-    
   }
 }
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('PawMart Server is Running');
+app.get("/", (req, res) => {
+  res.send("PawMart Server is Running");
 });
-
 
 app.listen(PORT, () => {
   console.log(`PawMart Server running on port: ${PORT}`);
