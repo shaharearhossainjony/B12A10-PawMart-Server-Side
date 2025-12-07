c99d16c4140d4fd5b348658784d478427f7574ab
shaharearhossainjony <shaharearhossainjony@gmail.com>
2025-12-07 20:48:22 +0600

    Basic server and mongoDB setup done with credentials hide by dot env

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@b12a10-server-cluster.hig91a4.mongodb.net/?appName=B12A10-Server-Cluster`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB successfully!");
+  } finally {
+    
+  }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('PawMart Server is Running');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`PawMart Server running on port: ${PORT}`);
+});
